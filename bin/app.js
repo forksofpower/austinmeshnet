9e673b6191bc07b3c91bca240db225257fbe5755
Patrick Jones <forksofpower@gmail.com>
2016-12-22 04:17:43 -0600

    basic app structure

---
+++ bin/app.js
@@ -0,0 +1,44 @@
+'use strict'
+
+let
+    express = require('express')
+  , path = require('path')
+  , favicon = require('serve-favicon')
+  , bodyParser = require('body-parser')
+  , index = require('./routes/index')
+  , newsletter = require('./routes/newsletter')
+  , app = express()
+  ;
+
+
+// setup views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', index)
+app.use('/newsletter', newsletter)
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+})
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
